6d552215c4e6487cc9cf186ca16543b27a404d8e
nerdbetter <jasontlogan@gmail.com>
2017-08-15 14:38:13 -0500

    added tests to check if errors fire for improper file names

--- lab-jason/test/test-joke.js
+++ lab-jason/test/test-joke.js
@@ -12,5 +12,29 @@ describe('joke', function(){
       });
       console.log('loads in order');
     });
+    it ('should call callback with err given bad file 1', function (done) {
+      sut.readJoke('two', 'two', 'three', (err, res) => {
+        assert.notStrictEqual(err, null);
+        assert.strictEqual(res, undefined);
+
+        done();
+      });
+    });
+    it ('should call callback with err given bad file 2', function (done) {
+      sut.readJoke('one', 'one', 'three', (err, res) => {
+        assert.notStrictEqual(err, null);
+        assert.strictEqual(res, undefined);
+
+        done();
+      });
+    });
+    it ('should call callback with err given bad file 3', function (done) {
+      sut.readJoke('one', 'two', 'two', (err, res) => {
+        assert.notStrictEqual(err, null);
+        assert.strictEqual(res, undefined);
+
+        done();
+      });
+    });
   });
 });
